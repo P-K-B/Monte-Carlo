f8346d3b23dd11c8fbb8135359f7b5f59268f195
FREEKILLER201020 <htc.burchenko@gmail.com>
2018-06-23 14:00:03 +0300

    clear

--- sketch.js
+++ sketch.js
@@ -1,12 +1,6 @@
-// 'use strict';
-var manag;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(14);
-  // create_control(40, 30);
-  // create_window(40, 220, 500, 500);
-  // create_log(600, 220, 400, 500);
   parameters = new parameters();
   buttons = new buttons();
   x = 50;
@@ -14,7 +8,4 @@ function setup() {
   y_w = windowHeight - y - 40;
   x_w = y_w;
   rect(x, y, x_w, y_w);
-  // manag.Draw_frame();
 }
-
-function draw() {}
